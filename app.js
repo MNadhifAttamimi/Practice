9d3866a55c01e7a926be8e8dcec12fc2dc58539b
Muhammad Nadhif <nadhif.ppqita@gmail.com>
2023-08-30 10:29:24 +0700

    membuat server

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+//example sederhana
+
+/**
+    req adalah singkatan dari request yg isinya yg dikirimkan oleh client.
+    seperti body, parametery, query
+    res adalah singkatan dari responss yg isinya kita kirim kje client.
+    contoh seperti data, json, html, dan codeHTTP (default:200)   
+    */
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// biar bisa running
+app.listen(3000, () => {
+    console.log('Hai Dev, Servermu udah jalan di localhost:3000')
+})
+//cara jalanin
+//jalaninnya dgn menulis node app.js di terminal
